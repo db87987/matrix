d6b3d27addc2fd2075c4ddb964526c9c3cf910d8
Dragon Black <db87987@gmail.com>
2019-04-24 11:33:01 +0300

    Add loopholes spec

---
+++ spec/parser/loopholes_parser_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'parser/loopholes_parser'
+require 'pry'
+
+describe LoopholesParser do
+  let(:raw_data) do
+    JSON.parse(File.read('spec/fixtures/parser_raw_data/loopholes_raw_data.json'))
+  end
+  let(:parsed_keys) { %i[source start_node end_node start_time end_time] }
+
+  it 'parses raw data' do
+    parsed_data = described_class.new(data: raw_data).parse
+    expect(parsed_data.length).to eq(3)
+    expect(parsed_data.first.keys).to eq(parsed_keys)
+  end
+end
